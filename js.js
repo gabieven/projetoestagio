9b61474c37f93f0a639a02c749f5aafc9ddb4411
Gabrielli Venerando <gabieven14@gmail.com>
2020-02-03 10:37:48 -0200

    Modificação k

--- js.js
+++ js.js
@@ -13,35 +13,43 @@ function openForm() {
      }
    }
 
+   let modalBtn = document.getElementById("modal-btn");
+   let modal = document.querySelector(".modal");
+   let closeBtn = document.querySelector(".close-btn");
+   let xit =  document.getElementById("f2");
+   let xet =  document.getElementById("f1");
+   //let modalBtn = document.querySelectorAll("modal-btn");
 
-   let modalBtn = document.getElementById("modal-btn")
-   let modal = document.querySelector(".modal")
-   let closeBtn = document.querySelector(".close-btn")
-   modalBtn.onclick = function(){
-     modal.style.display = "block"
+   modalBtn.onclick = function()
+   {
+     modal.style.display = "block";
+     xit.style.display = "block";
    }
    closeBtn.onclick = function(){
-     modal.style.display = "none"
+     modal.style.display = "none";
    }
    window.onclick = function(e){
      if(e.target == modal){
-       modal.style.display = "none"
+       modal.style.display = "none";
      }
    }
 
 
 
-   let modalBtnn = document.getElementById("modal-btnn")
-   let modall = document.querySelector(".modall")
-   let closeBtnn = document.querySelector(".close-btnn")
-   modalBtnn.onclick = function(){
-     modall.style.display = "block"
-   }
-   closeBtnn.onclick = function(){
-     modall.style.display = "none"
-   }
-   window.onclick = function(e){
-     if(e.target == modall){
-       modall.style.display = "none"
-     }
-   }
+   // let modalBtnn = document.getElementById("modal-btnn")
+   // let modall = document.querySelector(".modall")
+   // let closeBtnn = document.querySelector(".close-btnn")
+   // modalBtnn.onclick = function(){
+   //   modall.style.display = "block"
+   // }
+   // closeBtnn.onclick = function(){
+   //   modall.style.display = "none"
+   // }
+   // window.onclick = function(e){
+   //   if(e.target == modall){
+   //     modall.style.display = "none"
+   //   }
+   // }
+
+
+// var x = document.getElementsByClassName('elemente');
